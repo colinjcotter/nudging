637d12c6b815c9f3d069b1d1d3c6e89730af7636
maneeshkrsingh <maneesh.sr51@gmail.com>
2024-05-20 16:58:27 +0100

    tidying up stochstic euler model

--- nudging/models/stochastic_euler.py
+++ nudging/models/stochastic_euler.py
@@ -5,25 +5,36 @@ import numpy as np
 
 
 class Euler_SD(base_model):
-    def __init__(self, n_xy_pts, nsteps, dt=0.5, lambdas=False, seed=12353):
+    def __init__(self, n_xy_pts, nsteps, dt,
+                 noise_scale, mesh=False,
+                 salt=False, lambdas=False, seed=12353):
         self.n = n_xy_pts
         self.nsteps = nsteps
         self.dt = dt
-        self.lambdas = lambdas  # include lambdas in allocate
+        self.noise_scale = noise_scale
+        self.mesh = mesh
+        self.salt = salt
+        self.lambdas = lambdas
         self.seed = seed
 
     def setup(self, comm=MPI.COMM_WORLD):
         r = 0.01
-        self.Lx = 2.0*fd.pi  # Zonal length
-        self.Ly = 2.0*fd.pi  # Meridonal length
-        self.mesh = fd.PeriodicRectangleMesh(self.n, self.n,
-                                             self.Lx, self.Ly,
-                                             direction="x",
-                                             quadrilateral=True,
-                                             comm=comm)
+        self.Lx = 1.0  # Zonal length
+        self.Ly = 1.0  # Meridonal length
+        if not self.mesh:
+            self.mesh = fd.UnitSquareMesh(self.n, self.n,
+                                          quadrilateral=True,
+                                          comm=comm)
+        x = fd.SpatialCoordinate(self.mesh)
+        dx = fd.dx
+        dS = fd.dS
         # FE spaces
         self.Vcg = fd.FunctionSpace(self.mesh, "CG", 1)  # Streamfunctions
         self.Vdg = fd.FunctionSpace(self.mesh, "DQ", 1)  # PV space
+        self.Vu = fd.VectorFunctionSpace(self.mesh, "DQ", 1) # velocity
+        self.u_vel = fd.Function(self.Vu)
+        self.phi_fn = fd.Function(self.Vcg)
+        self.phi_mod_fn = fd.Function(self.Vcg)
 
         self.q0 = fd.Function(self.Vdg)
         self.q1 = fd.Function(self.Vdg)
@@ -36,11 +47,9 @@ class Euler_SD(base_model):
         self.psi0 = fd.Function(self.Vcg)
 
         # Build the weak form for the inversion
-        from firedrake import inner, grad, dx
-        Apsi = (inner(grad(psi), grad(phi)) + psi*phi)*dx
+        Apsi = (fd.inner(fd.grad(psi), fd.grad(phi)))*dx
         Lpsi = -self.q1 * phi * dx
-
-        bc1 = fd.DirichletBC(self.Vcg, 0.0, (1, 2))
+        bc1 = fd.DirichletBC(self.Vcg, fd.zero(), ("on_boundary"))
 
         psi_problem = fd.LinearVariationalProblem(Apsi, Lpsi,
                                                   self.psi0,
@@ -50,51 +59,74 @@ class Euler_SD(base_model):
               "pc_factor_mat_solver_type": "mumps"}
         self.psi_solver = fd.LinearVariationalSolver(psi_problem,
                                                      solver_parameters=sp)
+        # Setup noise term using Matern formula
         self.W_F = fd.FunctionSpace(self.mesh, "DG", 0)
         self.dW = fd.Function(self.W_F)
-
         dW_phi = fd.TestFunction(self.Vcg)
-        dw = fd.TrialFunction(self.Vcg)
-        self.alpha_w = fd.CellVolume(self.mesh)
+        dU = fd.TrialFunction(self.Vcg)
 
-        # to store noise data
-        du_w = fd.Function(self.Vcg)
+        cell_area = fd.CellVolume(self.mesh)
+        #alpha_w =(1/cell_area**0.5)
+        kappa_inv_sq =fd.Constant(1/30**2)
 
-        bcs_dw = fd.DirichletBC(self.Vcg,  fd.zero(), ("on_boundary"))
-        a_dW = inner(grad(dw), grad(dW_phi))*dx + dw*dW_phi*dx
-        L_dW = self.dW*dW_phi*dx
+        self.dU_1 = fd.Function(self.Vcg)
+        self.dU_2 = fd.Function(self.Vcg)
+        self.dU_3 = fd.Function(self.Vcg)
 
-        dW_problem = fd.LinearVariationalProblem(a_dW, L_dW, du_w,
-                                                 bcs=bcs_dw)
-        self.dW_solver = fd.LinearVariationalSolver(dW_problem,
-                                                    solver_parameters=sp)
-
+        # zero boundary condition for noise
+        bc = fd.DirichletBC(self.Vcg, 0, "on_boundary")
+        a_dW = kappa_inv_sq*fd.inner(fd.grad(dU), fd.grad(dW_phi))*dx \
+            + dU*dW_phi*dx
+        L_w1 = self.dW*dW_phi*dx
+        w_prob1 = fd.LinearVariationalProblem(a_dW, L_w1, self.dU_1, bcs=bc,
+                                              constant_jacobian=True)
+        self.wsolver1 = fd.LinearVariationalSolver(w_prob1,
+                                                   solver_parameters=sp)
+        L_w2 = self.dU_1*dW_phi*dx
+        w_prob2 = fd.LinearVariationalProblem(a_dW, L_w2, self.dU_2, bcs=bc,
+                                              constant_jacobian=True)
+        self.wsolver2 = fd.LinearVariationalSolver(w_prob2,
+                                                   solver_parameters=sp)
+        L_w3 = self.dU_2*dW_phi*dx
+        w_prob3 = fd.LinearVariationalProblem(a_dW, L_w3, self.dU_3, bcs=bc,
+                                              constant_jacobian=True)
+        self.wsolver3 = fd.LinearVariationalSolver(w_prob3,
+                                                   solver_parameters=sp)
         # Add noise with stream function to get stochastic velocity
-        from firedrake import dot, jump, as_vector
-        psi_mod = self.psi0+du_w
+        Dt = self.dt
+        if self.salt:
+            psi_mod = self.psi0*Dt+self.noise_scale*self.dU_1*Dt**0.5  # SALT noise
+        else:
+            psi_mod = self.psi0*Dt
+
+        #print(type(psi_mod))
+        self.psi_mod = psi_mod
+        #print(type(self.psi_mod))
 
         def gradperp(u):
-            return as_vector((-u.dx(1), u.dx(0)))
+            return fd.as_vector((-u.dx(1), u.dx(0)))
         self.gradperp = gradperp
         # upwinding terms
         n_F = fd.FacetNormal(self.mesh)
-        un = 0.5 * (dot(gradperp(psi_mod), n_F) +
-                    abs(dot(gradperp(psi_mod), n_F)))
+        un = 0.5 * (fd.dot(gradperp(psi_mod), n_F) +
+                    abs(fd.dot(gradperp(psi_mod), n_F)))
 
         q = fd.TrialFunction(self.Vdg)
         p = fd.TestFunction(self.Vdg)
         Q = fd.Function(self.Vdg)
-
+        Q.interpolate(0.1*fd.sin(8*fd.pi*x[0]))
         # timestepping equation
-        from firedrake import dS
         a_mass = p*q*dx
-        a_int = (dot(grad(p), -q*gradperp(psi_mod)) - p*(Q-r*q)) * dx
-        a_flux = (dot(jump(p), un("+") * q("+") - un("-") * q("-"))) * dS
-        arhs = a_mass - self.dt * (a_int + a_flux)
+        a_int = (fd.dot(fd.grad(p), -q*gradperp(psi_mod)) - Dt*p*(Q-r*q))*dx
+        if not self.salt:
+            a_int += Dt*p*self.noise_scale*self.dU_3*Dt**0.5*dx
+        a_flux = (fd.dot(fd.jump(p), un("+")*q("+") - un("-")*q("-")))*dS
+        arhs = a_mass - (a_int + a_flux)
+
 
         q_prob = fd.LinearVariationalProblem(a_mass,
                                              fd.action(arhs, self.q1),
-                                             self.dq1)
+                                             self.dq1, constant_jacobian=True)
         dgsp = {"ksp_type": "preonly",
                 "pc_type": "bjacobi",
                 "sub_pc_type": "ilu"}
@@ -110,6 +142,7 @@ class Euler_SD(base_model):
         xv, yv = np.meshgrid(x_point, y_point)
         x_obs_list = np.vstack([xv.ravel(), yv.ravel()]).T.tolist()
         VOM = fd.VertexOnlyMesh(self.mesh, x_obs_list)
+        self.VVOM_out = fd.VectorFunctionSpace(VOM.input_ordering, "DG", 0)
         self.VVOM = fd.VectorFunctionSpace(VOM, "DG", 0)
 
     def run(self, X0, X1):
@@ -122,25 +155,37 @@ class Euler_SD(base_model):
             self.dW.assign(self.X[step+1])
             if self.lambdas:
                 self.dW += self.X[step+1+self.nsteps]*(self.dt)**0.5
-            self.dW_solver.solve()
+            # solve  dW --> dU0 --> dU1 --> dU3
+            self.wsolver1.solve()
+            self.wsolver2.solve()
+            self.wsolver3.solve()
 
-            # Compute the streamfunction for the known value of q0
+
             self.q1.assign(self.q0)
             self.psi_solver.solve()
+            
+            self.u_vel.project(self.gradperp(self.psi_mod))
             self.q_solver.solve()
 
             # Find intermediate solution q^(1)
             self.q1.assign(self.dq1)
             self.psi_solver.solve()
+            self.u_vel.project(self.gradperp(self.psi_mod))
             self.q_solver.solve()
+            
 
             # Find intermediate solution q^(2)
-            self.q1.assign(0.75 * self.q0 + 0.25 * self.dq1)
+            self.q1.assign((3/4)*self.q0 + (1/4)*self.dq1)
             self.psi_solver.solve()
+            self.phi_fn.project(self.dt*self.psi0)
+            self.phi_mod_fn.project(self.psi0*self.dt+self.noise_scale*self.dU_3*self.dt**0.5)
+            
+            
+            self.u_vel.project(self.gradperp(self.psi_mod))
             self.q_solver.solve()
 
             # Find new solution q^(n+1)
-            self.q0.assign(self.q0/3 + 2 * self.dq1/3)
+            self.q0.assign((1/3)*self.q0 + (2/3)*self.dq1)
         X1[0].assign(self.q0)  # save sol at the nstep th time
 
     # control PV
@@ -182,13 +227,14 @@ class Euler_SD(base_model):
     def lambda_functional(self):
         nsteps = self.nsteps
         dt = self.dt
+        dx = fd.dx
+        cv = fd.CellVolume(self.mesh)
         for step in range(nsteps):
             lambda_step = self.X[nsteps + 1 + step]
             dW_step = self.X[1 + step]
-            dx = fd.dx
             dlfunc = fd.assemble(
-                (1/self.alpha_w)*lambda_step**2*dt/2*dx
-                - (1/self.alpha_w)*lambda_step*dW_step*dt**0.5*dx)
+                (1/cv)*lambda_step**2*dt/2*dx
+                - (1/cv)*lambda_step*dW_step*dt**0.5*dx)
             if step == 0:
                 lfunc = dlfunc
             else:
