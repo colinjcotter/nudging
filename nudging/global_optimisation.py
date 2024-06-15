84546426d873518c7ee9bfef287fcfc224018fd1
Colin Cotter <colin.cotter@imperial.ac.uk>
2024-06-15 14:45:46 +0100

    sketching setting up the PETSc vec

--- nudging/global_optimisation.py
+++ nudging/global_optimisation.py
@@ -1,107 +1,46 @@
 import firedrake as fd
-from pyop2.mpi import MPI
-import warnings
+from operator import mul
+import petsc4py.PETSc as PETSc
+from pyadjoint.enlisting import Enlist
 
-class optimisation_vector(object):
-    def __init__(self, allocate, ensemble):
+class ensemble_petsc_interface:
+    def init__(self, X, ensemble):
         """
-        A class for doing optimisation
-        on lists of pyadjoint overloaded types. We provide a minimal
-        amount of inner product space operations required for optimisation
-        algorithms.
+        Build a PETSc vec over the global communicator
+        using an example set of list of overloaded types
+        (one for each ensemble rank)
 
-        allocate - a method/function that allocates a new list of the 
-        required types.
-        ensemble - a Firedrake Ensemble object for ensemble MPI communication.
+        inputs:
+        X - a list of overloaded types
+        ensemble - Firedrake.Ensemble ensemble communication object
         """
-        self.allocate = allocate
-        self.vec = allocate()
+
+        # use X and ensemble to compute size and distribution of
+        # PETSc vec
+
+        X = Enlist(X)
         self.ensemble = ensemble
 
-    def copy(self, scale=1):
-        vec = self.allocate()
-        for i, item in enumerate(self.vec):
-            if isinstance(item, float):
-                vec[i] = scale*self.vec[i]
-            elif isinstance(item, Fd.Function):
-                vec[i].assign(scale*self.vec[i])
-            else:
-                raise NotImplementedError("This type of vector is not supported.")
-        return vec
-    
-    def __iadd__(self, other):
-        for i, item in enumerate(self.vec):
-            if isinstance(item, float):
-                self.vec[i] += other[i]
-            elif isinstance(item, Fd.Function):
-                self.vec[i] += other[i]
-            else:
-                raise NotImplementedError("This type of vector is not supported.")
-        return self
+        # WE ARE GOING TO CHEAT AND USE A MIXED FUNCTION SPACE TO
+        # STORE ALL THE ENSEMBLE LOCAL DATA THIS MEANS THAT THIS WILL
+        # ONLY WORK IF ALL X ARE FIREDRAKE FUNCTIONS
 
-    def __imul__(self, other):
-        for i, item in enumerate(self.vec):
-            if isinstance(item, float):
-                self.vec[i] *= other
-            elif isinstance(item, Fd.Function):
-                self.vec[i] *= other
-            else:
-                raise NotImplementedError("This type of vector is not supported.")
-        return self
+        function_spaces = []
+        for x in X:
+            fn = x.tape_value()
+            if not isinstance(fn, fd.Function):
+                raise NotImplementedError("Controls must be Firedrake Functions")
+            function_spaces.append(fn.ufl_function_space())
+        # This will flatten mixed spaces into one mixed space
+        mixed_function_space = mul(function_spaces)
+        w = fd.Function(mixed_function_space)
 
-    def inner(self, other):
-        val = 0.
-        for i, item in enumerate(self.vec):
-            if isinstance(item, float):
-                val += self.vec[i]*other[i]
-            elif isinstance(item, Fd.Function):
-                inner = fd.inner(self.vec[i], other[i])
-                val += fd.assemble(inner*fd.dx)
-            else:
-                raise NotImplementedError("This type of vector is not supported.")
-        # allreduce over communicator
-        ensemble_comm = self.ensemble.ensemble_comm
-        val = ensemble_comm.allreduce(sendobj=local_functional, op=MPI.SUM)
-        return val
-
-    def norm(self):
-        return (self.inner(self, self))**0.5
-
-class gradient_descent_solver(object):
-    def __init__(self, allocate, J, gain):
-        """
-        A class implementing a basic gradient descent optimisation solver
-        for EnsembleReducedFunctional.
-
-        allocate - a method/function that allocates a new list of the 
-        required types that are provided to f.
-        J - the EnsembleReducedFunctional
-        gain - the gain/damping parameter/learning rate for the
-        gradient descent.
-        """
-        self.allocate = allocate
-        self.J = J
-        self.gain = gain
-
-    def minimise(self, x, tol=1.0e-3, maxits=1000, verbose=False):
-        """
-        Minimise via gradient descent until the norm of the gradient
-        is below the tolerance, or the maximum number of iterations is reached.
-
-        x - an optimisation_vector containing the initial guess which
-        will be overwritten with the gradient descent steps.
-        tol - the tolerance.
-        maxits - the maximum number of iterations.
-        """
-        X = optimisation_vector(x)
-        self.J(X.vec)
-        # actually returns a gradient
-        der = optimisation_vector(self.J.derivative())
-        residual = der.norm()
-        for i in range(maxits):
-            if residual < tol:
-                break
-            X -= der*self.gain
-            der = optimisation_vector(self.J.derivative())
-        if i == maxits - 1 and verbose:
-            warnings.warn("Maximum iterations reached before reaching tolerance in gradient descent.")
+        # sniff the sizes to create the global PETSc Vec
+        with w.dat.vec_ro as wvec:
+            local_size = wvec.local_size
+            global_size = wvec.size
+        sizes = (local_size, global_size)
+        with w.dat.vec as wvec:
+            self.Vec = PETSc.Vec().createWithArray(wvec.array,
+                                                   size=sizes,
+                                                   comm=ensemble.global_comm)
