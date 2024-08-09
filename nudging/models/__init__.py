fc17ea7949f08358fb66032bac4f444c6e2bf39d
Colin Cotter <colin.cotter@imperial.ac.uk>
2024-08-09 14:11:21 +0100

    init file

--- nudging/models/__init__.py
+++ nudging/models/__init__.py
@@ -1,3 +1,4 @@
 from .stochastic_Camassa_Holm import *  # noqa: F401, F403
 from .sim_model import *  # noqa: F401, F403
 from .linear_scalar_sde import *  # noqa: F401, F403
+from .stochastic_KS import *  # noqa: F401, F403
