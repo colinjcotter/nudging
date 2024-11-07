046ad11ced315a9d870b90d20efec0144739fb20
maneeshkrsingh <maneesh.sr51@gmail.com>
2024-11-07 15:22:19 +0000

    clean assimilate.py

--- examples/stochastic_KS/assimilate.py
+++ examples/stochastic_KS/assimilate.py
@@ -18,14 +18,20 @@ dt = params["dt"]
 nu = params["nu"]
 dc = params["dc"]
 
-verbose = True
-model = KS_CIP(nsteps, xpoints, seed=12353, lambdas=False,
-               dt=dt, nu=nu, dc=dc, L=L)
-jtfilter = ndg.jittertemp_filter(n_jitt=4, delta=0.1, verbose=verbose)
+nensemble = [10]*1
 
-nensemble = [20]*1
+model = KS_CIP(nsteps, xpoints, seed=12353, lambdas=True,
+               dt=dt, nu=nu, dc=dc, L=L)
+
+nudging = True
+jtfilter = ndg.jittertemp_filter(n_jitt=0, delta=0.15,
+                             verbose=2, MALA=False,
+                             visualise_tape=False, nudging=True, sigma=0.01)
+
+
 
 jtfilter.setup(nensemble, model)
+
 comm = jtfilter.subcommunicators.comm
 ecomm = jtfilter.subcommunicators.ensemble_comm
 # load the initial ensemble
@@ -56,48 +62,18 @@ y_e_list = []
 y_sim_obs_list = []
 for m in range(y.shape[1]):
     y_e_shared = ndg.SharedArray(partition=nensemble,
-                                 comm=jtfilter.subcommunicators.ensemble_comm)
-    ecomm = jtfilter.subcommunicators.ensemble_comm
-    y_sim_obs_shared = ndg.SharedArray(partition=nensemble,
-                                       comm=ecomm)
+                                 comm=ecomm)
     y_e_list.append(y_e_shared)
-    y_sim_obs_list.append(y_sim_obs_shared)
 
 ys = y.shape
 if fd.COMM_WORLD.rank == 0:
     y_e = np.zeros((np.sum(nensemble), ys[0], ys[1]))
-    y_e_asmfwd = np.zeros((np.sum(nensemble), ys[0], ys[1]))
-    y_sim_obs_alltime_step = np.zeros((np.sum(nensemble), nsteps, ys[1]))
-    y_sim_obs_allobs_step = np.zeros((np.sum(nensemble), nsteps*N_obs, ys[1]))
 
 # do assimiliation step
 for k in range(N_obs):
     PETSc.Sys.Print("Step", k)
     yVOM.dat.data[:] = y[:,k]
 
-    # make a copy so that we don't overwrite the initial condition
-    # in the next step
-    for i in range(nensemble[jtfilter.ensemble_rank]):
-        for p in range(len(jtfilter.new_ensemble[i])):
-            jtfilter.new_ensemble[i][p].assign(jtfilter.ensemble[i][p])
-        model.randomize(jtfilter.new_ensemble[i])
-
-    # Compute simulated observations using "prior" distribution
-    # i.e. before we have used the observed data
-    for step in range(nsteps):
-        for i in range(nensemble[jtfilter.ensemble_rank]):
-            model.run(jtfilter.new_ensemble[i], jtfilter.new_ensemble[i])
-            # note, not safe in spatial parallel
-            fwd_simdata = model.obs().dat.data[:]
-            for m in range(y.shape[1]):
-                y_sim_obs_list[m].dlocal[i] = fwd_simdata[m]
-
-        for m in range(y.shape[1]):
-            y_sim_obs_list[m].synchronise()
-            if fd.COMM_WORLD.rank == 0:
-                y_sim_obs_alltime_step[:, step, m] = y_sim_obs_list[m].data()
-                y_sim_obs_allobs_step[:, nsteps*k+step, m] = \
-                    y_sim_obs_alltime_step[:, step, m]
 
     # actually do the data assimilation step
     jtfilter.assimilation_step(yVOM, log_likelihood)
@@ -114,8 +90,8 @@ for k in range(N_obs):
             y_e[:, k, m] = y_e_list[m].data()
 
 if fd.COMM_WORLD.rank == 0:
-    print("Time shape", y_sim_obs_alltime_step.shape)
-    print("Obs shape", y_sim_obs_allobs_step.shape)
-    print("Ensemble member", y_e.shape)
-    np.save("assimilated_ensemble.npy", y_e)
-    np.save("simualated_all_time_obs.npy", y_sim_obs_allobs_step)
+    print("ensemble shape", y_e.shape)
+    if not nudging:
+        np.save("tempjitt_assimilated_Vorticity_ensemble.npy", y_e)
+    if nudging:
+        np.save("nudge_assimilated_Vorticity_ensemble.npy", y_e)
